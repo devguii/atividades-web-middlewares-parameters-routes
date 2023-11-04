874be05fa8935bc111fcbba9844df85f4cf90a44
gui <guigon@alunos.utfpr.edu.br>
2023-11-04 14:34:23 -0300

    feat: add ex03

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 const atv1Router = require("./routes/atv1");
 const atv2Router = require("./routes/atv2");
+const atv3Router = require("./routes/atv3");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/atv1", atv1Router);
 app.use("/atv2", atv2Router);
+app.use("/atv3", atv3Router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
