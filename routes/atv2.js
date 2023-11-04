62b8570b9dbce6af61cd8e6d82c9ec1e36bb4e33
gui <guigon@alunos.utfpr.edu.br>
2023-11-04 14:21:57 -0300

    fix: ex02 redirect

--- routes/atv2.js
+++ routes/atv2.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-let ultima_rota = "a";
+var ultima_rota = "b";
 
 router.get("/", (req, res) => {
   if (ultima_rota == "a") {
     ultima_rota = "b";
-    res.redirect("/ativ1/b");
+    res.redirect("/atv1/b");
   } else {
     ultima_rota = "a";
-    res.redirect("/ativ1/a");
+    res.redirect("/atv1/a");
   }
 });
 
