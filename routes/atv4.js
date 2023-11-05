0e2c757e7dfd4255df9fefde58c1110ae2a47243
gui <guigon@alunos.utfpr.edu.br>
2023-11-05 01:04:35 -0300

    fix: ex04 page

--- routes/atv4.js
+++ routes/atv4.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.end(
+    "Informe um texto apos a url para inverte-lo (.../atv4/*TEXTO AQUI*)"
+  );
+});
+
 router.get("/:texto", (req, res) => {
   let texto = req.params.texto || "";
   if (texto == "") {
